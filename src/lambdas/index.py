059fc3912ec83dfc0cbaf75502c6e74ce913411e
Alexander Fandos <alex.fandos@gmail.com>
2023-02-11 20:10:19 +0100

    deploy api gateway testing

--- src/lambdas/index.py
+++ src/lambdas/index.py
@@ -1,8 +1,24 @@
 import json
 
 def lambda_handler(event, context):
-    print("Hello from Lambda!")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    transactionId = event['queryStringParameters']['transactionId']
+    transactionType = event['queryStringParameters']['type']
+    transactionAmount = event['queryStringParameters']['amount']
+
+    print('transactionId=' + transactionId)
+    print('transactionType=' + transactionType)
+    print('transactionAmount=' + transactionAmount)
+
+    transactionResponse = {}
+    transactionResponse['transactionId'] = transactionId
+    transactionResponse['type'] = transactionType
+    transactionResponse['amount'] = transactionAmount
+
+
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] = {}
+    responseObject['headers']['Content-Type'] = 'application/json'  
+    responseObject['body'] = json.dumps(transactionResponse)
+
+    return responseObject
